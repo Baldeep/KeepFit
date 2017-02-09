77a54ced8713c0327aae464f026d53acbbaeed39
Baldeep Singh <Baldeep Singh>
2017-02-09 15:54:37 +0000

    starting to add the add goal screen

---
+++ app/src/main/java/cs551/baldeep/keepfit/AddGoal.java
@@ -0,0 +1,20 @@
+package cs551.baldeep.keepfit;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by balde on 09/02/2017.
+ */
+
+public class AddGoal extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_add_goal);
+
+        Button addButton = (Button) findViewById(R.id.)
+    }
+}
